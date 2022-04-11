e883f6e57cb22befa4aabc8e3c40052e47505e41
Samin1615088 <samin.shafkat@gmail.com>
2022-04-12 04:39:52 +0600

    router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProjectScreen from "./screens/ProjectScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProjectScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,6 +7,7 @@ import {
   Nav,
   NavDropdown,
   Navbar,
+  NavLink,
 } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -14,25 +15,31 @@ import {
   faCartShopping,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">E-Bazar</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>E-Bazar</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#cart">
-                <FontAwesomeIcon icon={faCartShopping} className="pe-2" />
-                Cart
-              </Nav.Link>
-
-              <Nav.Link href="#login">
-                <FontAwesomeIcon icon={faUser} className="pe-2" />
-                Login
-              </Nav.Link>
+              <LinkContainer to="cart">
+                <NavLink>
+                  <FontAwesomeIcon icon={faCartShopping} className="pe-2" />
+                  Cart
+                </NavLink>
+              </LinkContainer>
+              <LinkContainer to="#login">
+                <NavLink>
+                  <FontAwesomeIcon icon={faUser} className="pe-2" />
+                  Login
+                </NavLink>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
